79662ea37ccefccb029b5250c315c575b6f3d3f1
julia <89081314528@mail.ru>
2020-03-26 21:04:21 +0300

    26/03 3

--- src/main/java/ru/julia/TaskStrangerSum.java
+++ src/main/java/ru/julia/TaskStrangerSum.java
@@ -1,12 +1,12 @@
 package ru.julia;
 
 /**
- * Для чисел от 1 до 100 посчитать сколько получится, если писать 1 - 2 + 3 - 4 и так далее.
+ * Для чисел от 1 до 100 посчитать сколько получится, если писать 1 + 2 - 3 + 4 и так далее.
  */
 public class TaskStrangerSum {
     public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 1; i < 101; i ++) {
+        int sum = 1;
+        for (int i = 2; i < 101; i ++) {
             if ( i % 2 == 0) {
                 sum = sum + i;
                 System.out.println(sum);
