b0c9bad97d9af28ec956ce844815196cbf032455
julia <89081314528@mail.ru>
2020-03-26 20:49:27 +0300

    26/03

--- src/main/java/ru/julia/TaskStrangerSum.java
+++ src/main/java/ru/julia/TaskStrangerSum.java
@@ -6,9 +6,15 @@ package ru.julia;
 public class TaskStrangerSum {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; i < 101; i = i + 2) {
-            sum = sum + i - (i + 1);
-            System.out.println(sum);
+        for (int i = 1; i < 101; i ++) {
+            if ( i % 2 == 0) {
+                sum = sum - i;
+                System.out.println(sum);
+            } else {
+                sum = sum + i;
+                System.out.println(sum);
+            }
         }
+        System.out.println(sum);
     }
 }
