ee440106ae278b0d6b7225fb0d046eb9704bf664
julia <89081314528@mail.ru>
2020-03-29 12:25:50 +0300

    29/03 2

---
+++ src/main/java/ru/julia/EasyNumbers.java
@@ -0,0 +1,22 @@
+package ru.julia;
+
+/**
+ * Простое число это такое число, которое не делится ни на какое другое число нацело.
+ * Написать программу, которая будет определять простое число в переменной или нет.
+ */
+
+public class EasyNumbers {
+    public static void main(String[] args) {
+        int chislo = 2;
+        int count = 0;
+        for (int i = 2; i < chislo; i++) {
+            if (chislo%i == 0) {
+                count = count + 1;
+            }
+        }
+        if (count > 0) {
+            System.out.println("Число не простое");
+        } else
+            System.out.println("Число простое");
+    }
+}
