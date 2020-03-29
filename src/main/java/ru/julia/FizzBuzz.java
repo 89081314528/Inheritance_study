2371be37c3b02cb8e002b11e042ff3a9295568cc
julia <89081314528@mail.ru>
2020-03-29 12:00:49 +0300

    29/03

---
+++ src/main/java/ru/julia/FizzBuzz.java
@@ -0,0 +1,21 @@
+package ru.julia;
+
+/**
+ * Написать программу, которая для каждой цифры в диапазоне от 0 до 100 печатает следующии
+ * Если цифра делится нацело на три - печатает fizz
+ * Если цифра делится нацело не пять - печатает buzz
+ * Если цифра делится и на три и на пять - печатает FizzBuzz
+ */
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + "fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println(i + "fizz");
+            } else if (i % 5 == 0) {
+                System.out.println(i + "buzz");
+            }
+        }
+    }
+}
