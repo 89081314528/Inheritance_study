b470a7afa37ce70bed88c0e346b139fa985501ff
julia <89081314528@mail.ru>
2020-04-05 19:56:02 +0300

    05/04 2

--- src/main/java/ru/julia/MortgageCalculator.java
+++ src/main/java/ru/julia/MortgageCalculator.java
@@ -14,24 +14,38 @@ public class MortgageCalculator {
         double loanTerm = 12;
         double monthlyPayment = loanDebt * monthInterestRate / (1 - Math.pow(1 + monthInterestRate, -loanTerm));
         System.out.println("Ежемесячный платеж " + Math.rint(100 * monthlyPayment) / 100);
-        System.out.println("Переплата по кредиту " + Math.rint(100 * (monthlyPayment * loanTerm - loanDebt) / 100));
+//        System.out.println("Переплата по кредиту " + Math.rint(100 * (monthlyPayment * loanTerm - loanDebt) / 100));
         double currentLoanAmount = loanDebt;
-        double oneTimeEarlyPaymentAmount = 5000;
-        double oneTimeEarlyPaymentMonth = 3; // 1 это февраль. предполагаем, что погашение происходит в начале месяца
+        double monthlyEarlyPayment = 1000;
+//        double oneTimeEarlyPaymentAmount = 5000;
+//        double oneTimeEarlyPaymentMonth = 3; // 1 это февраль. предполагаем, что погашение происходит в начале месяца
         for (int i = 0; i < loanTerm; i++) {
-            if (i == oneTimeEarlyPaymentMonth) {
-                monthlyPayment = (currentLoanAmount - oneTimeEarlyPaymentAmount) * monthInterestRate /
-                        (1 - Math.pow(1 + monthInterestRate, -(loanTerm - i)));
-                System.out.println("Досрочное погашение " + oneTimeEarlyPaymentAmount);
-                System.out.println("Новый ежемесячный платеж " + Math.rint(100 * monthlyPayment) / 100);
-                currentLoanAmount = currentLoanAmount - oneTimeEarlyPaymentAmount;
-            }
+//            if (i == oneTimeEarlyPaymentMonth) {
+//                monthlyPayment = (currentLoanAmount - oneTimeEarlyPaymentAmount) * monthInterestRate /
+//                        (1 - Math.pow(1 + monthInterestRate, -(loanTerm - i)));
+//                System.out.println("Досрочное погашение " + oneTimeEarlyPaymentAmount);
+//                System.out.println("Новый ежемесячный платеж " + Math.rint(100 * monthlyPayment) / 100);
+//                currentLoanAmount = currentLoanAmount - oneTimeEarlyPaymentAmount;
+//            }
             double currentInterestPayment = currentLoanAmount * monthInterestRate;
             double currentLoanPayment = monthlyPayment - currentInterestPayment;
             currentLoanAmount = currentLoanAmount - currentLoanPayment;
-            System.out.println("Ежемесячный платеж " + Math.rint(100 * monthlyPayment) / 100 + " в т. ч проценты "
+            System.out.println("Внесен ежемесячный платеж " + Math.rint(100 * monthlyPayment) / 100 + " в т. ч проценты "
                     + Math.rint(100 * currentInterestPayment) / 100 + " основной долг " +
                     Math.rint(100 * currentLoanPayment) / 100);
+            System.out.println("Остаток основного долга " + Math.rint(100 * currentLoanAmount) / 100);
+
+            if (currentLoanAmount < monthlyEarlyPayment) {
+                System.out.println("Досрочное погашение " + Math.rint(100 * currentLoanAmount) / 100);
+                break;
+            }
+            currentLoanAmount = currentLoanAmount - monthlyEarlyPayment;
+            monthlyPayment = currentLoanAmount * monthInterestRate /
+                    (1 - Math.pow(1 + monthInterestRate, -(loanTerm - i - 1)));
+            System.out.println("Досрочное погашение " + Math.rint(100 * monthlyEarlyPayment) / 100);
+            System.out.println("Остаток основного долга " + Math.rint(100 * currentLoanAmount) / 100);
+            System.out.println("Новый ежемесячный платеж " + Math.rint(100 * monthlyPayment) / 100);
+
         }
     }
 }
