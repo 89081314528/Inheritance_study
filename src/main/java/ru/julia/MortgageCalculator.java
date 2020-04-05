5892c23acee6330e455f2f9f403109cbb9f0da74
julia <89081314528@mail.ru>
2020-04-05 17:47:35 +0300

    05/04

--- src/main/java/ru/julia/MortgageCalculator.java
+++ src/main/java/ru/julia/MortgageCalculator.java
@@ -7,21 +7,30 @@ package ru.julia;
  * вывод - проценты, долг, платеж и сумма досрочного погашения в этом месяце
  */
 public class MortgageCalculator {
-    private static Math Precision;
     public static void main(String[] args) {
         int loanDebt = 12000;
         double interestRate = 0.1;
         double monthInterestRate = interestRate / 12;
-        double loanTerm = 36;
+        double loanTerm = 12;
         double monthlyPayment = loanDebt * monthInterestRate / (1 - Math.pow(1 + monthInterestRate, -loanTerm));
         System.out.println("Ежемесячный платеж " + Math.rint(100 * monthlyPayment) / 100);
         System.out.println("Переплата по кредиту " + Math.rint(100 * (monthlyPayment * loanTerm - loanDebt) / 100));
         double currentLoanAmount = loanDebt;
+        double oneTimeEarlyPaymentAmount = 5000;
+        double oneTimeEarlyPaymentMonth = 3; // 1 это февраль. предполагаем, что погашение происходит в начале месяца
         for (int i = 0; i < loanTerm; i++) {
+            if (i == oneTimeEarlyPaymentMonth) {
+                monthlyPayment = (currentLoanAmount - oneTimeEarlyPaymentAmount) * monthInterestRate /
+                        (1 - Math.pow(1 + monthInterestRate, -(loanTerm - i)));
+                System.out.println("Досрочное погашение " + oneTimeEarlyPaymentAmount);
+                System.out.println("Новый ежемесячный платеж " + Math.rint(100 * monthlyPayment) / 100);
+                currentLoanAmount = currentLoanAmount - oneTimeEarlyPaymentAmount;
+            }
             double currentInterestPayment = currentLoanAmount * monthInterestRate;
             double currentLoanPayment = monthlyPayment - currentInterestPayment;
             currentLoanAmount = currentLoanAmount - currentLoanPayment;
-            System.out.println("Проценты " + Math.rint(100 * currentInterestPayment) / 100 + " Долг " +
+            System.out.println("Ежемесячный платеж " + Math.rint(100 * monthlyPayment) / 100 + " в т. ч проценты "
+                    + Math.rint(100 * currentInterestPayment) / 100 + " основной долг " +
                     Math.rint(100 * currentLoanPayment) / 100);
         }
     }
